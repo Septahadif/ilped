1ad6d1bd8f0919ca19650b5cda836a933d1b6c24
Septahadif <110327728+Septahadif@users.noreply.github.com>
2025-09-13 14:59:51 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,16 +1,14 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+export default {
+  async fetch(request, env, ctx) {
+    const upgrade = request.headers.get("Upgrade");
+    if (upgrade !== "websocket") {
+      return new Response("Expected WebSocket", { status: 400 });
+    }
 
-async function handleRequest(request) {
-  const upgradeHeader = request.headers.get("Upgrade");
-  if (!upgradeHeader || upgradeHeader !== "websocket") {
-    return new Response("Expected WebSocket", { status: 400 });
+    const url = new URL(request.url);
+    url.hostname = "id.masjawa.my.id"; // ganti dengan domain VPS-mu
+    url.protocol = "https:";
+
+    return fetch(new Request(url, request));
   }
-
-  const webSocket = await fetch("wss://id.masjawa.my.id:443/vmess", {
-    headers: request.headers,
-  });
-
-  return webSocket;
 }
